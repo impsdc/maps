24fbf5a58e9832261432dbf732fdd12fcae689f2
santamaria <pauliix.santamaria@gmail.com>
2020-04-27 18:52:08 +0200

    polyline working

--- app.js
+++ app.js
@@ -20,12 +20,11 @@ function add(e) {
   let $check = $adress.indexOf($value);
 
   if ($check === -1) {
-    console.log("go");
+    //cheking if the address is already in
     $adress.push($value);
     $location.innerHTML = "";
     geocode($adress, $value);
   } else {
-    console.log(yes);
     $error.innerHTML = "the adress is already in";
   }
 }
@@ -43,28 +42,29 @@ async function geocode($list, $value) {
       //formatted Adress
       console.log(response);
       if (response.data.results == 0) {
+        // checking if the address is valid
         let pos = $adress.indexOf($value);
         let remove = $adress.splice(pos, 1);
         $error.innerHTML = "L'adress n'est pas valide";
       } else {
         let formattedAdress = response.data.results[0].formatted_address;
-
+        let mainAdress =
+          response.data.results[0].address_components[0].long_name;
         let lat = response.data.results[0].geometry.location.lat;
         let lng = response.data.results[0].geometry.location.lng;
 
         let formattedAddressOutput = `<li class='list-group-item'>
-                    adress : ${formattedAdress}<span class="ml-3 btn btn-danger">D</span>
+                    adress : ${formattedAdress}<span class="ml-3 btn btn-danger" id="suppr">D</span>
                 </li>`;
 
         document.getElementById(
           "formattedAdresse"
         ).innerHTML += formattedAddressOutput;
 
-        //initialize markes array
+        //initialize markers array
         var marker = {
           coords: { lat: lat, lng: lng },
-          icon:
-            "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+          title: mainAdress,
         };
         markers.push(marker);
 
@@ -72,14 +72,12 @@ async function geocode($list, $value) {
       }
     })
     .catch(function (error) {
-      //   $error.innerHTML = "Adresse pas valide";
       console.log(error);
     });
 }
 
 //initialize the map
 const push = ($list) => {
-  console.log($list);
   var options = {
     zoom: 1,
     center: { lat: 48.856614, lng: 2.3522219 },
@@ -88,7 +86,7 @@ const push = ($list) => {
   var map = new google.maps.Map(document.getElementById("map"), options);
 
   //in case of just one address
-  $list.map((n) => initMap(markers, $list, map));
+  $list.map((n) => initMap(markers, map));
 
   //if there are more than one adress, set up a line
   if ($list.length > 1) {
@@ -97,7 +95,7 @@ const push = ($list) => {
       $coords.push(markers[i].coords);
     }
     var options = {
-      zoom: 1,
+      zoom: 4,
       center: markers[1].coords,
     };
 
@@ -110,13 +108,11 @@ const push = ($list) => {
       strokeOpacity: 1.0,
       strokeWeight: 2,
     });
-    console.log(line);
     line.setMap(map);
-    // map.fitBounds(bounds);
   }
 };
 
-const initMap = (markers, list, map) => {
+const initMap = (markers, map) => {
   let coords = markers[markers.length - 1].coords;
 
   var options = {
@@ -136,8 +132,8 @@ const initMap = (markers, list, map) => {
     var marker = new google.maps.Marker({
       position: props.coords,
       map: map,
-      icon: props.icon,
       title: props.title,
+      animation: google.maps.Animation.DROP,
     });
 
     // if title is not null
@@ -152,4 +148,35 @@ const initMap = (markers, list, map) => {
   }
 };
 
-const moyenne = () => {};
+// function calculateAndDisplayRoute(
+//   directionsService,
+//   directionsDisplay,
+//   pointA,
+//   pointB
+// ) {
+//   directionsService.route(
+//     {
+//       origin: pointA,
+//       destination: pointB,
+//       travelMode: google.maps.TravelMode.DRIVING,
+//     },
+//     function (response, status) {
+//       if (status == google.maps.DirectionsStatus.OK) {
+//         directionsDisplay.setDirections(response);
+//       } else {
+//         window.alert("Directions request failed due to " + status);
+//       }
+//     }
+//   );
+// }
+//   // Instantiate a directions service.
+//   directionsService = new google.maps.DirectionsService();
+//   directionsDisplay = new google.maps.DirectionsRenderer({
+//   map: map,
+//   });
+//     calculateAndDisplayRoute(
+//       directionsService,
+//       directionsDisplay,
+//       $coords[0],
+//       $coords[1]
+//     );
